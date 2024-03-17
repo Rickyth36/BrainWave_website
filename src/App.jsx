d53d4fd61b061312251f9fdd3f8d8c2ce9c56b13
Ricky <rickythoudam123@gmail.com>
2024-03-17 20:24:09 +0530

    Finished Collaboration section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benifits from "./components/Benifits";
+import Collaboration from "./components/Collaboration";
 // import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -7,7 +9,9 @@ function App() {
     <div>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
-        <Hero/>
+        <Hero />
+        <Benifits />
+        <Collaboration/>
       </div>
       <ButtonGradient />
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -74,7 +74,7 @@ const Hero = () => {
                 />
                 <Generating
                   className="absolute left-4 right-4
-                bttom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2 "
+                bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2 "
                 />
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul
